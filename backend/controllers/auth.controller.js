14e452a19d07bab213797227547d1dee61d378db
FerminEncalada <jose.f.encalada.l@unl.edu.ec>
2025-10-13 16:11:42 -0500

    token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,7 +6,6 @@ export const register = async (req, res) => {
     const { email, username, password } = req.body;
 
     try {
-
     const passwordHash = await bcrypt.hash(password, 10)
 
     const newUser = new User({ 
