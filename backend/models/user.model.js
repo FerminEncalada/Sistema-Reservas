882a995b4b8f64313fb9abe893eb84a685b46b44
FerminEncalada <jose.f.encalada.l@unl.edu.ec>
2025-10-12 21:41:45 -0500

    guardado de datos en DB

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -17,6 +17,8 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     }
+}, {
+    timestamps: true
 })
 
 export default mongoose.model('User', userSchema);
