f68de38f889740a5fb74c677aeb21fd655b0544d
FerminEncalada <jose.f.encalada.l@unl.edu.ec>
2025-10-13 22:08:55 -0500

    correcciones

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import { register, login, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
@@ -9,14 +8,5 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.post("/logout", logout);
-=======
-import { register, login } from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post('/register', register);
-
-router.post('/login', login);
->>>>>>> origin/develop
 
 export default router;
