8920368c4e1403abbc54dbc9c2980a26b5f3ceb0
FerminEncalada <jose.f.encalada.l@unl.edu.ec>
2025-10-13 22:31:36 -0500

    cambio

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { createAccessToken } from '../libs/jwt.js';
 
+
 export const register = async (req, res) => {
     const { email, username, password } = req.body;
 
